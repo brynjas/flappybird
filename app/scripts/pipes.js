6fe226ee57408ef5bdc905bca0059e563677cdb8
brynjas <brynja13@ru.is>
2016-04-07 16:23:58 +0000

    efri súla

--- app/scripts/pipes.js
+++ app/scripts/pipes.js
@@ -2,10 +2,10 @@ window.Pipes = (function() {
     'use strict';
 
     var SPEED = 20;
-    var GAP = 15;
+    var GAP = 20;
 
     
-     var PipeEl = function(pipe, x, y) {
+    var PipeEl = function(pipe, x, y) {
         this.pipe = pipe;
         this.pos = {
             x: x,
@@ -13,15 +13,18 @@ window.Pipes = (function() {
         };
     };
 
+    var originalPipeGap = getRandomInt(7.5, 35);
+
     var Pipes = function(el, game) {
         this.el = el;
         this.game = game;
         
-        this.pipeObj1 = {top: new PipeEl(this.el.find('.top1'), 30, 45),
-    					 bottom: new PipeEl(this.el.find('.bottom1'), 25, 45)};
+        this.pipeObj1 = {top: new PipeEl(this.el.find('.top1'), this.game.WORLD_WIDTH + 15, originalPipeGap),
+    					 bottom: new PipeEl(this.el.find('.bottom1'), this.game.WORLD_WIDTH + 15, originalPipeGap + GAP)};
         //this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translateY(' + this.pipeObj1.top.pos.y + 'em)');
         //this.pipeObj1.bottom.pipe.css('transform', 'translateZ(0) translateY(' + this.pipeObj1.bottom.pos.y + 'em)');
     };
+
 
 	/**
 	 * Resets the state of the player for a new game.
@@ -42,9 +45,9 @@ window.Pipes = (function() {
         // var worldHeight = this.game.WORLD_HEIGHT;
 
         console.log("Player-x:" + position.x + " y:" + position.y);
-        console.log("BottomPipe-x:" + this.pipeObj1.bottom.pos.x + " y:" + this.pipeObj1.bottom.pos.y);
+        console.log("TopPipe-x:" + this.pipeObj1.top.pos.x + " y:" + this.pipeObj1.top.pos.y);
 
- 		this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.top.pos.x + 'em, ' + this.pipeObj1.top.pos.y + "em)");
+ 		this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.top.pos.x + 'em, ' + (this.pipeObj1.top.pos.y - 69.5) + "em)");
         this.pipeObj1.bottom.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.bottom.pos.x + "em, " + this.pipeObj1.bottom.pos.y + "em)");
 
            
@@ -56,7 +59,7 @@ window.Pipes = (function() {
             this.pipeObj1.top.pos.y = pipeGap;
             this.pipeObj1.bottom.pos.y = pipeGap + GAP;
 
-            this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.top.pos.x + 'em, ' + this.pipeObj1.top.pos.y + "em)");
+            this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.top.pos.x + 'em, ' + (this.pipeObj1.top.pos.y - 69.5) + "em)");
             this.pipeObj1.bottom.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.bottom.pos.x + "em, " + this.pipeObj1.bottom.pos.y + "em)");
 
             //this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.top.pos.y + 'em)');
@@ -68,6 +71,12 @@ window.Pipes = (function() {
         if(position.x + 2.5 >= (this.pipeObj1.bottom.pos.x - 2.15) &&
                 position.x - 2.5 <= (this.pipeObj1.bottom.pos.x + 2.15) &&
                     position.y + 2.5 >= (this.pipeObj1.bottom.pos.y - 2.5)) {
+            return this.game.gameover();
+        }
+
+        if(position.x + 2.5 >= (this.pipeObj1.top.pos.x - 2.15) &&
+                position.x - 2.5 <= (this.pipeObj1.top.pos.x + 2.15) &&
+                    position.y - 2.5 <= (this.pipeObj1.top.pos.y + 2.5)) {
             return this.game.gameover();
         }
              
