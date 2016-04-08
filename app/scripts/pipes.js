f70473627f08ee4a8dea8b5ba04b3d24d12f5cf6
atligudmunds <a13@ru.is>
2016-04-08 14:55:23 +0000

    Added many pipes

--- app/scripts/pipes.js
+++ app/scripts/pipes.js
@@ -3,6 +3,7 @@ window.Pipes = (function() {
 
     var SPEED = 20;
     var GAP = 20;
+    //var pipeInterval = this.game.WORLD_WIDTH / 3;
 
     
     var PipeEl = function(pipe, x, y) {
@@ -21,6 +22,22 @@ window.Pipes = (function() {
         
         this.pipeObj1 = {top: new PipeEl(this.el.find('.top1'), this.game.WORLD_WIDTH + 15, originalPipeGap),
     					 bottom: new PipeEl(this.el.find('.bottom1'), this.game.WORLD_WIDTH + 15, originalPipeGap + GAP)};
+
+        this.pipeArr = [
+                        {top: new PipeEl(this.el.find('.top1'), this.game.WORLD_WIDTH + 15, originalPipeGap),
+                         bottom: new PipeEl(this.el.find('.bottom1'), this.game.WORLD_WIDTH + 15, originalPipeGap + GAP)},
+
+                         {top: new PipeEl(this.el.find('.top2'), this.game.WORLD_WIDTH + 15 + (this.game.WORLD_WIDTH / 3), originalPipeGap),
+                         bottom: new PipeEl(this.el.find('.bottom2'), this.game.WORLD_WIDTH + 15 + (this.game.WORLD_WIDTH / 3), originalPipeGap + GAP)},
+
+                         {top: new PipeEl(this.el.find('.top3'), this.game.WORLD_WIDTH + 15 + 2*(this.game.WORLD_WIDTH / 3), originalPipeGap),
+                         bottom: new PipeEl(this.el.find('.bottom3'), this.game.WORLD_WIDTH + 15 + 2*(this.game.WORLD_WIDTH / 3), originalPipeGap + GAP)}
+                         ];
+
+        this.pipeTest = [{top: "turd", bottom: "nugget"},
+                        {top: "furb", bottom: "cracker"},
+                        {top: "herb", bottom: "bisquit"}];
+
       };
 	/**
 	 * Resets the state of the player for a new game.
@@ -28,6 +45,8 @@ window.Pipes = (function() {
 	Pipes.prototype.reset = function() {
 
         console.log("er í pipe reset");
+
+        console.log("Pipe1: " + this.pipeTest[0].top);
 
         this.pipeObj1.top.pos.x = this.game.WORLD_WIDTH + 15;
         this.pipeObj1.top.pos.y = originalPipeGap;
@@ -47,20 +66,50 @@ window.Pipes = (function() {
 
 
 	Pipes.prototype.onFrame = function(delta, position) {
-		
+		/*
         this.pipeObj1.top.pos.x -= delta * SPEED;
-        this.pipeObj1.bottom.pos.x -= delta * SPEED;  
-        var pipeGap = getRandomInt(7.5, 35);
+        this.pipeObj1.bottom.pos.x -= delta * SPEED;
+*/      for(var i = 0; i < 3; i++) {
+            var pipeGap = getRandomInt(7.5, 35);
+            this.pipeArr[i].top.pos.x -= delta * SPEED;
+            this.pipeArr[i].bottom.pos.x -= delta * SPEED;
+
+            this.pipeArr[i].top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeArr[i].top.pos.x + 'em, ' + (this.pipeArr[i].top.pos.y - 69.5) + "em)");
+            this.pipeArr[i].bottom.pipe.css('transform', 'translateZ(0) translate(' + this.pipeArr[i].bottom.pos.x + "em, " + this.pipeArr[i].bottom.pos.y + "em)");
+
+            if(this.pipeArr[i].top.pos.x < -10) {
+
+                this.pipeArr[i].top.pos.x = this.game.WORLD_WIDTH + 5;
+                this.pipeArr[i].bottom.pos.x = this.game.WORLD_WIDTH + 5;
+
+                this.pipeArr[i].top.pos.y = pipeGap;
+                this.pipeArr[i].bottom.pos.y = pipeGap + GAP;
+
+                this.pipeArr[i].top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeArr[i].top.pos.x + 'em, ' + (this.pipeArr[i].top.pos.y - 69.5) + "em)");
+                this.pipeArr[i].bottom.pipe.css('transform', 'translateZ(0) translate(' + this.pipeArr[i].bottom.pos.x + "em, " + this.pipeArr[i].bottom.pos.y + "em)");
+            }
+
+            if(position.x + 2.5 >= (this.pipeArr[i].bottom.pos.x - 2.15) &&
+                    position.x - 2.5 <= (this.pipeArr[i].bottom.pos.x + 2.15) &&
+                        position.y + 2.5 >= (this.pipeArr[i].bottom.pos.y - 2.5)) {
+                return this.game.gameover();
+            }
+
+            if(position.x + 2.5 >= (this.pipeArr[i].top.pos.x - 2.15) &&
+                    position.x - 2.5 <= (this.pipeArr[i].top.pos.x + 2.15) &&
+                        position.y - 2.5 <= (this.pipeArr[i].top.pos.y + 2.5)) {
+                return this.game.gameover();
+            }
+        }
         // var worldHeight = this.game.WORLD_HEIGHT;
 
-       // console.log("Player-x:" + position.x + " y:" + position.y);
+        // console.log("Player-x:" + position.x + " y:" + position.y);
         //console.log("TopPipe-x:" + this.pipeObj1.top.pos.x + " y:" + this.pipeObj1.top.pos.y);
-
+/*
  		this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.top.pos.x + 'em, ' + (this.pipeObj1.top.pos.y - 69.5) + "em)");
         this.pipeObj1.bottom.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.bottom.pos.x + "em, " + this.pipeObj1.bottom.pos.y + "em)");
-
-           
-        if(this.pipeObj1.top.pos.x < -10) {
+*/         
+        /*if(this.pipeObj1.top.pos.x < -10) {
 
             this.pipeObj1.top.pos.x = this.game.WORLD_WIDTH + 5;
             this.pipeObj1.bottom.pos.x = this.game.WORLD_WIDTH + 5;
@@ -70,14 +119,14 @@ window.Pipes = (function() {
 
             this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.top.pos.x + 'em, ' + (this.pipeObj1.top.pos.y - 69.5) + "em)");
             this.pipeObj1.bottom.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.bottom.pos.x + "em, " + this.pipeObj1.bottom.pos.y + "em)");
-
+*/
             //this.pipeObj1.top.pipe.css('transform', 'translateZ(0) translate(' + this.pipeObj1.top.pos.y + 'em)');
             //this.pipeObj1.bottom.pipe.css('transform', 'translateZ(0) translateY(' + this.pipeObj1.bottom.pos.y + 'em)');
             //this.pipeObj1.top.pipe.css('height', this.pipeObj1.top.pos.y + 'em');
             //this.pipeObj1.bottom.pipe.css('height', this.pipeObj1.bottom.pos.y + 'em');
-        }
+        //}
 
-        if(position.x + 2.5 >= (this.pipeObj1.bottom.pos.x - 2.15) &&
+        /*if(position.x + 2.5 >= (this.pipeObj1.bottom.pos.x - 2.15) &&
                 position.x - 2.5 <= (this.pipeObj1.bottom.pos.x + 2.15) &&
                     position.y + 2.5 >= (this.pipeObj1.bottom.pos.y - 2.5)) {
             return this.game.gameover();
@@ -87,7 +136,7 @@ window.Pipes = (function() {
                 position.x - 2.5 <= (this.pipeObj1.top.pos.x + 2.15) &&
                     position.y - 2.5 <= (this.pipeObj1.top.pos.y + 2.5)) {
             return this.game.gameover();
-        }
+        }*/
              
     }
    
